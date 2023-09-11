9b135f6ab67a0a89ae19cfceb522e874b8e60baa
Your Name <ketaki9823@gmail.com>
2023-09-12 00:43:21 +0530

    update

--- app2.py
+++ app2.py
@@ -32,14 +32,14 @@ if (selected == "About"):
     st.title("Parkinson's Disease ")
     st.write("Parkinson's disease is a progressive disorder that affects the nervous system and the parts of the body controlled by the nerves. Symptoms start slowly. The first symptom may be a barely noticeable tremor in just one hand. Tremors are common, but the disorder may also cause stiffness or slowing of movement.")
     
-    img1 = Image.open("images.jpg")
+    img1 = Image.open("Parkinson-s-Disease-Detection-System-main\images.jpg")
    
     
     st.write("In the early stages of Parkinson's disease, your face may show little or no expression. Your arms may not swing when you walk. Your speech may become soft or slurred. Parkinson's disease symptoms worsen as your condition progresses over time.")
     st.write("Although Parkinson's disease can't be cured, medications might significantly improve your symptoms. Occasionally, your health care provider may suggest surgery to regulate certain regions of your brain and improve your symptoms.")
     
     st.header("Symptoms")
-    img = Image.open("parkinsons-disease-symptoms-infographic-1200x628.webp")
+    img = Image.open("Parkinson-s-Disease-Detection-System-main\Symptoms.jpg")
     
     st.write("Parkinson's disease signs and symptoms can be different for everyone. Early signs may be mild and go unnoticed. Symptoms often begin on one side of the body and usually remain worse on that side, even after symptoms begin to affect the limbs on both sides")
     st.image(img )
